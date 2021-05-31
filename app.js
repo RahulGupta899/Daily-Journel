05e061cde9da64e33f9ee49ff0c1039e98c0851f
Rahul Gupta <rgupta.jan@gmail.com>
2021-05-31 11:06:01 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,85 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
+mongoose.connect("mongodb+srv://admin-Rahul:RahulPRO2015@cluster0.iqtk2.mongodb.net/blogDB",{useNewUrlParser:true});
+
+const postSchema = new mongoose.Schema({
+  title:String,
+  body:String
+});
+const Post = mongoose.model("Post",postSchema);
+
+
+const homeStartingContent = "Keeping a daily journel can give you tremendous powers. Track you daily actions by writing down key lessons learned throughout the day cus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+
+app.get('/post/:topic', function (req, res) {
+  let postName = _.capitalize(req.params.topic);
+  console.log(postName);
+  Post.findOne({title:postName},function(err,result){
+    if(result){
+      res.render("post",{
+        title:result.title,
+        body:result.body
+      });
+    }
+  });
+});
+
+app.get("/",function(req,res){
+  Post.find(function(err,dbData){
+    if(!err){
+      res.render("home",{
+        content:homeStartingContent,
+        posts:dbData
+      });
+    }
+  });
+
+});
+
+app.get("/about",function(req,res){
+  res.render("about",{
+    content:aboutContent
+  })
+});
+
+app.get("/contact",function(req,res){
+  res.render("contact",{
+    content:contactContent
+  })
+});
+
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+  //new Document
+  let newTitle = _.capitalize(req.body.postTitle);
+  let newBody = req.body.postBody;
+
+  const newPost = new Post({
+    title: newTitle,
+    body: newBody
+  });
+  newPost.save();
+
+  res.redirect("/");
+});
